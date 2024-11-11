4d5034fb47146ac0f6bc6ae45f14327905a693cf
Sergio Dobri <sergiodobri@gmail.com>
2024-11-11 14:33:35 -0300

    Update basico.js

--- 1-introducao/basico.js
+++ 1-introducao/basico.js
@@ -1,6 +1,6 @@
 "use strict";
 // Links Úteis
-// http://www.typescriptlang.org/
+// http://www.typescriptlang.org/t*2iS&u@fh/cir*jXNByQ.6{?
 // http://www.typescriptlang.org/play/index.html
 const a = 'Teste TS!';
 console.log(a);
